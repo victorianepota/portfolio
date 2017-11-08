83cadb03b52fd861ab3a60d68f371b783fbdd1a9
Victoria Nepota <v.nepota@gmail.com>
2017-11-08 18:47:02 +0200

    update project

---
+++ scripts/hamburger.js
@@ -0,0 +1,12 @@
+/**
+ * Created by vnepo on 031 31.10.2017.
+ */
+<<<<<<< Updated upstream
+=======
+$(function () {
+    console.log('ham');
+    $('._hamburger').click(function () {
+        $('._nav').slideToggle();
+    });
+});
+>>>>>>> Stashed changes
